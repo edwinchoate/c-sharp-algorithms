3e918259f6ad7782d4648da8d1932436f6b086b7
Edwin Choate <edwin.choate@pm.me>
2024-09-30 13:48:27 -0400

    1.1 Validate strings in C#

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/01/01_01/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/01/01_01/start/Algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Algorithms
 {
@@ -6,7 +7,36 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine(IsUppercase("Hello World"));
+            Console.WriteLine(IsUppercase("HELLO WORLD"));
+            Console.WriteLine(IsUppercase("HELLOWORLD"));
+
+            Console.WriteLine();
+            
+            Console.WriteLine(IsLowercase("hello world"));
+            Console.WriteLine(IsLowercase("helloworld"));
+
+            Console.WriteLine();
+
+            Console.WriteLine(IsPasswordComplex("hello world"));
+            Console.WriteLine(IsPasswordComplex("hEllo world"));
+            Console.WriteLine(IsPasswordComplex("hell0 world"));
+            Console.WriteLine(IsPasswordComplex("hEll0 world"));
+        }
+
+        static Boolean IsPasswordComplex (string password) 
+        {
+            return password.Any(char.IsNumber) && password.Any(char.IsUpper) && password.Any(char.IsLower);
+        }
+
+        static Boolean IsUppercase (string s) 
+        {
+            return s.All(char.IsUpper); // .All is from System.Linq
+        }
+
+        static Boolean IsLowercase (string s) 
+        {
+            return s.All(char.IsLower); // .All is from System.Linq
         }
 
     }
--- notes.md
+++ notes.md
@@ -1,2 +1,30 @@
 # Notes
 
+## Introduction
+
+**Time Complexities**
+
+* O(1) - constant time
+* O(log(n)) - logarithmic time
+* O(n) - linear time
+* O(n<sup>2</sup>) - exponent time
+* O(n!) - factorial time
+
+## Ch. 1 String Algorithms
+
+`char` has some useful methods that become helpful when using `System.Linq` to search through strings.
+* `IsUpper`
+* `IsLower`
+* `IsDigit`
+* `IsLetter`
+* `IsNumber`
+
+Examples:
+
+```C#
+bool hasCapital = someString.Any(char.IsUpper); // check if a char in string is uppercase
+bool hasNumber = someString.Any(char.IsNumber);
+
+bool allCapital = someString.All(char.IsUpper); // checks if all chars in string are uppercase
+bool allNumbers = someString.All(char.IsNumber);
+```
