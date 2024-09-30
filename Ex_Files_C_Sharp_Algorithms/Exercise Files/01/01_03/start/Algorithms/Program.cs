29f306d54033eff502ce8c9f805b30b918db3b21
Edwin Choate <edwin.choate@pm.me>
2024-09-30 16:02:17 -0400

    1.3 Parse and search strings in C#

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/01/01_03/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/01/01_03/start/Algorithms/Program.cs
@@ -7,7 +7,26 @@ namespace Algorithms
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World".ToLower().Contains("ll"));
+
+            Console.WriteLine(IsAtEvenIndex("hello", 'h'));
+            Console.WriteLine(IsAtEvenIndex("hello", 'e'));
+            Console.WriteLine(IsAtEvenIndex("hello", 'l'));
+            Console.WriteLine(IsAtEvenIndex("hello", 'l'));
+            Console.WriteLine(IsAtEvenIndex("hello", 'o'));
+        }
+
+        static bool IsAtEvenIndex (string s, char c) 
+        {
+            if (s == null || s == String.Empty)
+                return false;
+
+            for (int i = 0; i < s.Length; i += 2) 
+            {
+                if (s[i] == c) return true; 
+            }
+            
+            return false;
         }
     }
 }
