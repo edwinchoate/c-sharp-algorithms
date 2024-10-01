bb87deaa9b7f0fdd3cf65a399136633d8f136334
Edwin Choate <edwin.choate@pm.me>
2024-10-01 08:47:02 -0400

    2.2 Binary search arrays in C#

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_02/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_02/start/Algorithms/Program.cs
@@ -4,11 +4,39 @@ namespace Algorithms {
     class Program {
 
         static Boolean BinarySearch(int[] inputArray, int item) {
+            if (inputArray == null || inputArray.Length == 0) 
+                return false; 
+
+            int i = 0, j = inputArray.Length - 1;
+            int midpoint;
+
+            while (i <= j) 
+            {
+                midpoint = (j + i) / 2;
+
+                if (inputArray[midpoint] == item) 
+                    return true;
+
+                if (item < inputArray[midpoint])
+                    j = midpoint - 1;
+                else 
+                    i = midpoint + 1;
+            }
+
             return false;
         }
 
         static void Main(string[] args) {
-            int[] arr = {1, 2, 3, 4, 5, 6 };
+            int[] arr1 = {1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(BinarySearch(arr1, 4));
+            Console.WriteLine(BinarySearch(arr1, 7));
+
+            int[] arr2 ={1, 2, 3, 4, 5, 6, 7};
+            Console.WriteLine(BinarySearch(arr2, 1));
+            Console.WriteLine(BinarySearch(arr2, 10));
+
+            int[] arr3 ={};
+            Console.WriteLine(BinarySearch(arr3, 5));
         }
     }
 }
