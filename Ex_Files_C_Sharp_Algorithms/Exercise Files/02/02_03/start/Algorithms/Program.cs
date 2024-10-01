8c3bdc1cfb354d63a95d3ca271f96e6f435e1cee
Edwin Choate <edwin.choate@pm.me>
2024-10-01 09:38:47 -0400

    2.3 Aggregate and filter arrays in C#

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_03/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/02/02_03/start/Algorithms/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Algorithms {
     class Program {
         static int[] FindEvenNums(int[] arr1, int[] arr2) {
-            return null;
+            List<int> list = new();
+
+            list.AddRange(arr1.Where(IsEven));
+            list.AddRange(arr2.Where(IsEven));
+
+            return list.ToArray();
+        }
+
+        static bool IsEven(int i)
+        {
+            return i % 2 == 0;
         }
 
         static void Main(string[] args) {
@@ -11,6 +23,8 @@ namespace Algorithms {
             int[] arr2 = {0, -2, -9, -39, 39, 10, 7};
 
             int[] evenArr = FindEvenNums(arr1, arr2);
+
+            Array.ForEach(evenArr, Console.WriteLine);
         }
     }
 }
