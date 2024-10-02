ae5085635e2a39e60b7c24a0480854381f0cc740
Edwin Choate <edwin.choate@pm.me>
2024-10-02 12:57:44 -0400

    4.8 Stack algorithms: Matching parentheses

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_08/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_08/start/Algorithms/Program.cs
@@ -4,8 +4,33 @@ using System.Collections.Generic;
 namespace Algorithms {
     class Program {
         
-         static Boolean hasMatchingParentheses(string s) {
-             return false;
+        static Boolean hasMatchingParentheses(string s) 
+        {
+            Stack<char> stack = new();
+
+            for (int i = 0; i < s.Length; i++) 
+            {
+                char current = s[i];
+
+                if (current == '(') 
+                {
+                    stack.Push(current);
+                    continue;
+                }
+
+                if (current == ')')
+                {
+                    if (stack.Count == 0)
+                        return false;
+                    else 
+                    {
+                        stack.Pop();
+                    }
+                }
+
+            }
+
+            return stack.Count == 0;
         }
 
         static void Main(string[] args) {
