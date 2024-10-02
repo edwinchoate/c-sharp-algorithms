22c973533746147cbee4f745cceb3c672aacbe49
Edwin Choate <edwin.choate@pm.me>
2024-10-02 17:47:15 -0400

    5.3 Leverage the HashSet type in C# algorithms

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_03/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_03/start/Algorithms/Program.cs
@@ -4,8 +4,34 @@ using System.Collections.Generic;
 namespace Algorithms {
     class Program {
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+            int[] a = {3, 2, 8, 4, 5};
+            int[] b = {5, 7, 3, 0, 2};
 
+            foreach (int num in FindMissingElements(a, b)) 
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        static List<int> FindMissingElements (int[] array1, int[] array2) 
+        {
+            HashSet<int> hashset = new();
+            List<int> missingElements = new();
+            
+            foreach (int num in array2) 
+            {
+                hashset.Add(num);
+            }
+
+            foreach (int num in array1) 
+            {
+                if (!hashset.Contains(num))
+                    missingElements.Add(num);
+            }
+
+            return missingElements;
         }
     }
 }
