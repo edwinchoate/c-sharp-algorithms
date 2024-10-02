630c8115affc000eb960aacf71403302a524b1ed
Edwin Choate <edwin.choate@pm.me>
2024-10-02 08:50:40 -0400

    4.3 Queue algorithms: Generate binary numbers

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_03/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_03/start/Algorithms/Program.cs
@@ -4,7 +4,26 @@ using System.Collections.Generic;
 namespace Algorithms {
     class Program {
         static void Main(string[] args) {
-            
+
+            PrintBinary(50);
+
         }
+
+        static void PrintBinary (int n) 
+        {
+            if (n <= 0) return;
+
+            Queue<int> queue = new();
+            queue.Enqueue(1);
+
+            for (int i = 0; i < n; i++) 
+            {
+                int current = queue.Dequeue();
+                Console.WriteLine(current);
+
+                queue.Enqueue(current*10);
+                queue.Enqueue(current*10 + 1);
+            }
+        } 
     }
 }
