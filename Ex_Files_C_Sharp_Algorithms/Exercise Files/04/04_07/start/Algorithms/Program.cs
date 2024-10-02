3e2217dc11d46e9867c5a3a0779a74fce58198d3
Edwin Choate <edwin.choate@pm.me>
2024-10-02 12:48:44 -0400

    4.7 Stack algorithms: Implementing next greater element

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_07/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/04/04_07/start/Algorithms/Program.cs
@@ -4,7 +4,43 @@ using System.Collections.Generic;
 namespace Algorithms {
     class Program {
         static void printNextGreaterElement(int[] arr) {
+            if (arr == null || arr.Length == 0) 
+            {
+                Console.WriteLine();
+                return;
+            }
 
+            Stack<int> stack = new();
+            stack.Push(arr[0]);
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int item = arr[i];
+
+                if (stack.Count == 0)
+                    stack.Push(item);
+                else 
+                {
+                    int popped = stack.Pop();
+
+                    while (popped < item) 
+                    {
+                        Console.WriteLine(popped + "-->" + item);
+                        if (stack.Count == 0) break;
+                        popped = stack.Pop();
+                    }
+
+                    if (popped > item)
+                        stack.Push(popped);
+                    
+                    stack.Push(item);
+                } 
+            }
+
+            while (stack.Count > 0) 
+            {
+                Console.WriteLine(stack.Pop() + "-->-1");
+            }
         }
 
         static void Main(string[] args) {
