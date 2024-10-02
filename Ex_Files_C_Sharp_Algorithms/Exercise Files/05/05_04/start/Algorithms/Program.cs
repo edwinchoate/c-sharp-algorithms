9cef54f51978ab2631b80f71d33ff21396156f7a
Edwin Choate <edwin.choate@pm.me>
2024-10-02 17:57:05 -0400

    5.4 Use the Dictionary type in C# algorithms

--- Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_04/start/Algorithms/Program.cs
+++ Ex_Files_C_Sharp_Algorithms/Exercise Files/05/05_04/start/Algorithms/Program.cs
@@ -5,6 +5,26 @@ namespace Algorithms {
     class Program {
 
         static void Main(string[] args) {
+            int[] a = {8, 3, 6, 23, 6, 3, 6, 57, 78, 3};
+            DisplayFrequenceOfEachElement(a);
+        }
+
+        static void DisplayFrequenceOfEachElement (int[] numbers) 
+        {
+            Dictionary<int, int> dict = new();
+
+            foreach (int num in numbers) 
+            {
+                if (dict.ContainsKey(num)) 
+                    dict[num]++;
+                else 
+                    dict.Add(num, 1);
+            }
+
+            foreach (int key in dict.Keys) 
+            {
+                Console.WriteLine("Number: {0}, Frequency: {1}", key, dict[key]);
+            }
         }
     }
 }
